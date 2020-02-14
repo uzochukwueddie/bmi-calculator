8e082a026f66a87bb4af48ec1781d0a391940403
Uzochukwu Eddie Odozi <uo@sms-digital.com>
2020-02-14 21:51:53 +0100

    Updates

---
+++ precache-manifest.2439ef5696010d1690e8c7002fe700dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a185139def9341442b1ff2aba12dce",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "7ca88cd567d544199074",
+    "url": "/bmi-calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "373dc6b257b22c9d69da",
+    "url": "/bmi-calculator/static/css/main.e0a46aff.chunk.css"
+  },
+  {
+    "revision": "7ca88cd567d544199074",
+    "url": "/bmi-calculator/static/js/2.61782553.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bmi-calculator/static/js/2.61782553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373dc6b257b22c9d69da",
+    "url": "/bmi-calculator/static/js/main.6587f67b.chunk.js"
+  },
+  {
+    "revision": "6604f1a013b6de023c9d",
+    "url": "/bmi-calculator/static/js/runtime-main.53120180.js"
+  }
+]);
